58079c0547aaf1aeea2f0b206d97e79162abe3eb
rigby200602 <tungle3456@gmail.com>
2025-08-26 22:45:59 +0700

    build 1st route

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {BrowserRouter}  from 'react-router-dom'
+import {BrowserRouter, Route, Routes}  from 'react-router-dom'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
+  <BrowserRouter>  
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
   </BrowserRouter>
 )
