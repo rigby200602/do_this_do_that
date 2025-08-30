eae2c203b7eec1e26bbee6184105d934d247ddd4
rigby200602 <tungle3456@gmail.com>
2025-08-30 22:23:33 +0700

    text size, pointer

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -6,10 +6,10 @@ function App() {
   return (
     <>
       <MainForm>
-        <h1 className='text-center py-12'>Do this do that</h1>
+        <h1 className='text-center py-12 text-3xl'>Do this do that</h1>
         <div className='flex w-[80%] mx-auto'>
-          <input type="text" placeholder='Enter your task here!' className='w-[80%] rounded-xl px-2 mx-2'/>
-          <button className='bg-blue-600 p-4 rounded-4xl w-[15%] text-white'>Add</button>
+          <input type="text" placeholder='Enter your task here!' className='w-[80%] rounded-xl px-2 mx-2 border-1'/>
+          <button className='bg-blue-600 p-4 rounded-4xl w-[15%] text-white cursor-pointer'>Add</button>
         </div>
       </MainForm>
     </>
