b4eff6540a3db63356780724e6cc43019a294e89
rigby200602 <tungle3456@gmail.com>
2025-08-30 22:15:14 +0700

    update css

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -6,7 +6,9 @@ function App() {
   return (
     <>
       <MainForm>
-        <h1 className='text-center'>Do this do that</h1>
+        <div className='w-full'>
+          <h1 className='text-center'>Do this do that</h1>
+        </div>
         <div className='flex w-[80%] mx-auto'>
           <input type="text" placeholder='Enter your task here!' className='w-[80%] rounded-xl px-2 mx-2'/>
           <button className='bg-blue-600 p-4 rounded-4xl w-[15%]'>Add</button>
--- frontEnd/src/components/MainForm.jsx
+++ frontEnd/src/components/MainForm.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MainForm = ({children}) => {
   return (
-    <div className='flex flex-col justify-center bg-gray-200 m-auto w-[50%] h-128 my-[6%] rounded-3xl shadow-lg text-black font-bold'>
+    <div className= 'bg-gray-200 mx-auto w-[50%] h-128 my-[6%] rounded-3xl shadow-lg text-black font-bold'>
         {children}
     </div>
   )
