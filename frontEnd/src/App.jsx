fd120a045f0d63d6c8ef812bc9d00fff60c8cb44
rigby200602 <tungle3456@gmail.com>
2025-08-30 22:03:38 +0700

    add add btn, title, fix css

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -6,7 +6,11 @@ function App() {
   return (
     <>
       <MainForm>
-        a
+        <h1 className='text-center'>Do this do that</h1>
+        <div className='flex w-[80%] mx-auto'>
+          <input type="text" placeholder='Enter your task here!' className='w-[80%] rounded-xl px-2 mx-2'/>
+          <button className='bg-blue-600 p-4 rounded-4xl w-[15%]'>Add</button>
+        </div>
       </MainForm>
     </>
   )
