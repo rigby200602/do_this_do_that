1be418e193e46bcef6163e4b7277e8a6bfa6bd9f
rigby200602 <tungle3456@gmail.com>
2025-08-30 22:18:34 +0700

    update css 1 more

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -6,9 +6,7 @@ function App() {
   return (
     <>
       <MainForm>
-        <div className='w-full'>
-          <h1 className='text-center'>Do this do that</h1>
-        </div>
+        <h1 className='text-center py-12'>Do this do that</h1>
         <div className='flex w-[80%] mx-auto'>
           <input type="text" placeholder='Enter your task here!' className='w-[80%] rounded-xl px-2 mx-2'/>
           <button className='bg-blue-600 p-4 rounded-4xl w-[15%]'>Add</button>
