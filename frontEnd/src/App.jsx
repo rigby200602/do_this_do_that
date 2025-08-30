01a1365d2a8b905989f104a2fd8c7af52c3180b2
rigby200602 <tungle3456@gmail.com>
2025-08-30 22:33:08 +0700

    text shadow

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <MainForm>
-        <h1 className='text-center py-12 text-3xl'>Do this do that</h1>
+        <h1 className='text-center py-12 text-3xl text-shadow-lg/20'>Do this do that</h1>
         <div className='flex w-[80%] mx-auto'>
           <input type="text" placeholder='Enter your task here!' className='w-[80%] rounded-xl px-2 mx-2 shadow-xl outline-0'/>
           <button className='bg-blue-600 p-4 rounded-4xl w-[15%] text-white cursor-pointer'>Add</button>
