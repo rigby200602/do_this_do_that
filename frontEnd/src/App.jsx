b4ecb14fb3dac0e9b916ddd5e48ce8310d86015b
rigby200602 <tungle3456@gmail.com>
2025-08-31 14:36:29 +0700

    fix value

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
         <h1 className='text-center py-12 text-3xl text-shadow-lg/20'>Do this do that</h1>
         <form className='flex w-[80%] mx-auto' onSubmit={onSubmitHandler}>
           <input type="text" placeholder='Enter your task here!' className='w-[80%] rounded-xl px-2 mx-2 shadow-xl outline-0'
+          value={task}
           onChange={e => setTask(e.target.value)}/>
           <button className='bg-blue-600 p-4 rounded-4xl w-[15%] text-white cursor-pointer' onClick={handleReset}>Add</button>
         </form>
