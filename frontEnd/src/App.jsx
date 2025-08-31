c32c8f79998b0476b9ee2f57de6b961fa063faaf
rigby200602 <tungle3456@gmail.com>
2025-08-31 14:14:05 +0700

    fix submitHandler

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -7,8 +7,9 @@ function App() {
   const onSubmitHandler = (e) => {
     try {
       e.preventDefault();
+      alert("Task added successfully!");
       setTask(task);
-
+      console.log(task);
     } catch (error) {
       console.log(error);
     }
@@ -17,10 +18,11 @@ function App() {
     <>
       <MainForm>
         <h1 className='text-center py-12 text-3xl text-shadow-lg/20'>Do this do that</h1>
-        <div className='flex w-[80%] mx-auto'>
-          <input type="text" placeholder='Enter your task here!' className='w-[80%] rounded-xl px-2 mx-2 shadow-xl outline-0'/>
-          <button className='bg-blue-600 p-4 rounded-4xl w-[15%] text-white cursor-pointer' onSubmit={onSubmitHandler}>Add</button>
-        </div>
+        <form className='flex w-[80%] mx-auto' onSubmit={onSubmitHandler}>
+          <input type="text" placeholder='Enter your task here!' className='w-[80%] rounded-xl px-2 mx-2 shadow-xl outline-0'
+          onChange={e => setTask(e.target.value)}/>
+          <button className='bg-blue-600 p-4 rounded-4xl w-[15%] text-white cursor-pointer' >Add</button>
+        </form>
       </MainForm>
     </>
   )
