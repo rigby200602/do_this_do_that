3ce3a09c3fdc1a059d597a676eb5ea4d41fdff16
rigby200602 <tungle3456@gmail.com>
2025-08-31 14:05:10 +0700

    try catch to see if there any error

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -5,8 +5,13 @@ import MainForm from './components/MainForm.jsx'
 function App() {
   const [task, setTask] = useState("");
   const onSubmitHandler = (e) => {
-    e.preventDefault();
-    setTask()
+    try {
+      e.preventDefault();
+      setTask(task);
+
+    } catch (error) {
+      console.log(error);
+    }
   }
   return (
     <>
@@ -14,7 +19,7 @@ function App() {
         <h1 className='text-center py-12 text-3xl text-shadow-lg/20'>Do this do that</h1>
         <div className='flex w-[80%] mx-auto'>
           <input type="text" placeholder='Enter your task here!' className='w-[80%] rounded-xl px-2 mx-2 shadow-xl outline-0'/>
-          <button className='bg-blue-600 p-4 rounded-4xl w-[15%] text-white cursor-pointer'>Add</button>
+          <button className='bg-blue-600 p-4 rounded-4xl w-[15%] text-white cursor-pointer' onSubmit={onSubmitHandler}>Add</button>
         </div>
       </MainForm>
     </>
